51f127f7f9a3fbe8865770a14c536f7f2b34dd74
jenkimm <jenniferxkim@gmail.com>
2015-08-22 18:31:15 -0700

    created transactions.js in controllers

--- models/Transaction.js
+++ models/Transaction.js
@@ -2,9 +2,11 @@ var bcrypt = require('bcrypt-nodejs');
 var crypto = require('crypto');
 var mongoose = require('mongoose');
 
-var transactionSchema = new mongoose.Schema({
+var TransactionSchema = new mongoose.Schema({
 	amount: Number,
 	_user: {type: mongoose.Schema.ObjectId, ref: 'User'},
 	_campaign: {type: mongoose.Schema.ObjectId, ref: 'Campaign'}
 
-})//end transactionSchema
+});//end transactionSchema
+
+mongoose.model('Transaction', TransactionSchema);
