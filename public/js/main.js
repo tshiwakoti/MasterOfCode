d161fbbe042b726fe919faa18a9a016dcf075886
Puru Dahal <pdahal@me.com>
2015-08-22 23:01:40 -0700

    Get rid of medium-editor for now

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,9 @@ $(document).ready(function() {
     typeSpeed: 70
   });
   
-  var elements = document.querySelectorAll('.editable', {
-    buttons: ['bold', 'italic', 'underline', 'anchor','h1', 'h2', 'h3', 'quote'],
-  }),
-  editor = new MediumEditor(elements);
+  var editor = new MediumEditor('.editable', {
+    placeholder: false
+  });
+  
+  // editor = new MediumEditor(elements);
 });
